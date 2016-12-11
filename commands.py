89d33ffd7b78f7d2e09ffb310d4b86e855eb0a7f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-12-11 09:57:15 +0000

    docstrings

--- commands.py
+++ commands.py
@@ -8,10 +8,22 @@ class CommandResponse:
 
 
 def command_help(event: dict, args: list) -> CommandResponse:
+    """
+    Constant command - return a vaguely useful string describing the bot's function.
+    :param event: Unused event data.
+    :param args: Unused argument list.
+    :return: A constant CommandResponse.
+    """
     return CommandResponse(True, "I'm a bot that detects low-quality contributions using machine-learning algorithms.")
 
 
 def command_alive(event: dict, args: list) -> CommandResponse:
+    """
+    Constant command - return a string, just to indicate that the bot's alive.
+    :param event: Unused event data.
+    :param args: Unused argument list.
+    :return: A constant CommandResponse.
+    """
     return CommandResponse(True, "You'll have to try harder than that to kill me.")
 
 
@@ -25,6 +37,14 @@ command_dict = {
 
 
 def execute(room: Room, event_data: dict, command_name: str, args: list) -> None:
+    """
+    Finds and runs a command specified by its name.
+    :param room: The ChatExchange Room that the command was run in.
+    :param event_data: The data field of a ChatExchange.chatexchange.events.Event.
+    :param command_name: The name of the command to execute, without the prefix.
+    :param args: A list of arguments to pass to the command.
+    :return: None
+    """
     global command_dict
     if command_name in command_dict:
         response = command_dict[command_name](event_data, args)
